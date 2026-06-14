81666f1c161c20226d32c8174cc2db1ade0397a1
fagner_de_paiva <fagnerdepaiva10@gmail.com>
2026-06-14 00:35:06 -0300

    DEPLOY

--- android/StickerTrack/app/src/main/java/com/example/stickertrack/network/RetrofitClient.kt
+++ android/StickerTrack/app/src/main/java/com/example/stickertrack/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
 
-    private const val BASE_URL = "http://192.168.0.107:8000/"
+    private const val BASE_URL = "https://stickertrack.onrender.com/"
 
     val api: ApiService by lazy {
 
